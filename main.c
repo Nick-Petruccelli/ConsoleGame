b90c6562a651c16283ae572841b8f6d3630707fb
NickPetruccelli <NickPetro18@gmail.com>
2025-08-05 13:34:02 -0400

    refactored to proporly implement opaque api with unity build

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "terminal_renderer.c"
+#include "console_game.c"
+
+int main(int argc, char** argv){
+}
---
+++ terminal_renderer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "mylib.h"
+
+typedef struct TerminalRendererHandel TerminalRendererHandel;
+
+
+TerminalRendererHandel *terminal_renderer_init(uint32 width, uint32 height);
+void terminal_renderer_shutdown(TerminalRendererHandel *terminal_renderer_h);
+uint32 terminal_renderer_load_sprite(TerminalRendererHandel *terminal_renderer_h, char* path);
+void terminal_renderer_blit_sprite(TerminalRendererHandel *terminal_rederer_h, uint32 sprite_id, uint32 x, uint32 y);
+void terminal_renderer_clear_window(TerminalRendererHandel *terminal_renderer_h);
+void terminal_renderer_print_frame(TerminalRendererHandel *terminal_renderer_h);
